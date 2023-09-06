4ca48306f81c5ef83fa4533cc8b1bb937be9c929
Teun Jansen <teunjansen91@gmail.com>
2023-09-06 19:18:14 +0200

    Questions array added

---
+++ script.js
@@ -0,0 +1,40 @@
+const questions = [
+  {
+    question: "What's the best sports club in the world?",
+    answers: [
+      { text: "AFC Ajax", correct: true },
+      { text: "FC Barcelona", correct: false },
+      { text: "Manchester United", correct: false },
+      { text: "FC Bayern Munchen", correct: false },
+    ],
+  },
+  {
+    question: "What's the most used programming language in the world?",
+    answers: [
+      { text: "Python", correct: false },
+      { text: "CSS", correct: false },
+      { text: "Javascript", correct: true },
+      { text: "C#", correct: false },
+    ],
+  },
+  {
+    question:
+      "After the GP of Monza 2023, how many times in a row did Max Verstappen that season?",
+    answers: [
+      { text: "15", correct: false },
+      { text: "7", correct: false },
+      { text: "3", correct: false },
+      { text: "10", correct: true },
+    ],
+  },
+  {
+    question:
+      "Which UCI championsship is considered the best in the world after the tour the France 2023?",
+    answers: [
+      { text: "UAE", correct: false },
+      { text: "Jumbo-Visma", correct: true },
+      { text: "Rabobank", correct: false },
+      { text: "FC Bayern Munchen", correct: false },
+    ],
+  },
+];
