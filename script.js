b5c3b20c64bf599b5569b061dc6c998e86f6648a
Teun Jansen <teunjansen91@gmail.com>
2023-09-07 04:12:31 +0200

    startQuiz() showQuestion() and resetState()

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const questions = [
   },
   {
     question:
-      "Which UCI championsship is considered the best in the world after the tour the France 2023?",
+      "Which UCI championship team is considered the best in the world after the tour the France 2023?",
     answers: [
       { text: "UAE", correct: false },
       { text: "Jumbo-Visma", correct: true },
@@ -38,3 +38,40 @@ const questions = [
     ],
   },
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
+
+startQuiz();
